c8a13c29b03ce65242c921a4a832cc945ce304dd
Eric Lee <eric.hyunil.lee@gmail.com>
2024-04-02 14:27:24 -0700

    Fixed folder structure, made the handlebars functional on homepage

--- controllers/homepageRoutes.js
+++ controllers/homepageRoutes.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 // Get method
+router.get('/', (req, res) => {
+	res.render('home')
+}
+)
 
 // Session Data
 router.get('/sessiondata', (req, res) => {
